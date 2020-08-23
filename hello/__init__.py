59d84b240c6495296ee7a57e6e915261e9659d91
Abyzab Zab <cassieburn1@gmail.com>
2020-08-23 19:56:30 +1200

    Pushing something

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from markupsafe import escape
 
 
@@ -32,5 +33,9 @@ def create_app():
     @app.route('/news', methods=['GET'])
     def shows_news():
         return "Today’s news is ..."
+
+    @app.route('/greeting')
+    def greeting():
+        return f'Salutations {request.args["name"]}'
 
     return app
